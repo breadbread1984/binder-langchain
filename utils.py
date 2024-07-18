cf9bd47afb2ab9b6b094ed1ae14dd9e88a924d74
breadbread1984 <breadbread1984@163.com>
2024-07-18 16:23:47 +0800

    update

--- utils.py
+++ utils.py
@@ -47,6 +47,170 @@ def sql_example(prompt_style, df, num_rows, few_shot_demonstration = True):
 
   return string
 
+def prepare_df_for_neuraldb_from_table(table: Dict, add_row_id=True, normalize=True, lower_case=True):
+    header, rows = table['header'], table['rows']
+    if add_row_id and 'row_id' not in header:
+        header = ["row_id"] + header
+        rows = [["{}".format(i)] + row for i, row in enumerate(rows)]
+    if normalize:
+        df = convert_df_type(pd.DataFrame(data=rows, columns=header), lower_case=lower_case)
+    else:
+        df = pd.DataFrame(data=rows, columns=header)
+
+    return df
+
+
+def convert_df_type(df: pd.DataFrame, lower_case=True):
+    """
+    A simple converter of dataframe data type from string to int/float/datetime.
+    """
+
+    def get_table_content_in_column(table):
+        if isinstance(table, pd.DataFrame):
+            header = table.columns.tolist()
+            rows = table.values.tolist()
+        else:
+            # Standard table dict format
+            header, rows = table['header'], table['rows']
+        all_col_values = []
+        for i in range(len(header)):
+            one_col_values = []
+            for _row in rows:
+                one_col_values.append(_row[i])
+            all_col_values.append(one_col_values)
+        return all_col_values
+
+    # Rename empty columns
+    new_columns = []
+    for idx, header in enumerate(df.columns):
+        if header == '':
+            new_columns.append('FilledColumnName')  # Fixme: give it a better name when all finished!
+        else:
+            new_columns.append(header)
+    df.columns = new_columns
+
+    # Rename duplicate columns
+    new_columns = []
+    for idx, header in enumerate(df.columns):
+        if header in new_columns:
+            new_header, suffix = header, 2
+            while new_header in new_columns:
+                new_header = header + '_' + str(suffix)
+                suffix += 1
+            new_columns.append(new_header)
+        else:
+            new_columns.append(header)
+    df.columns = new_columns
+
+    # Recognize null values like "-"
+    null_tokens = ['', '-', '/']
+    for header in df.columns:
+        df[header] = df[header].map(lambda x: str(None) if x in null_tokens else x)
+
+    # Convert the null values in digit column to "NaN"
+    all_col_values = get_table_content_in_column(df)
+    for col_i, one_col_values in enumerate(all_col_values):
+        all_number_flag = True
+        for row_i, cell_value in enumerate(one_col_values):
+            try:
+                float(cell_value)
+            except Exception as e:
+                if not cell_value in [str(None), str(None).lower()]:
+                    # None or none
+                    all_number_flag = False
+        if all_number_flag:
+            _header = df.columns[col_i]
+            df[_header] = df[_header].map(lambda x: "NaN" if x in [str(None), str(None).lower()] else x)
+
+    # Normalize cell values.
+    for header in df.columns:
+        df[header] = df[header].map(lambda x: str_normalize(x))
+
+    # Strip the mis-added "01-01 00:00:00"
+    all_col_values = get_table_content_in_column(df)
+    for col_i, one_col_values in enumerate(all_col_values):
+        all_with_00_00_00 = True
+        all_with_01_00_00_00 = True
+        all_with_01_01_00_00_00 = True
+        for row_i, cell_value in enumerate(one_col_values):
+            if not str(cell_value).endswith(" 00:00:00"):
+                all_with_00_00_00 = False
+            if not str(cell_value).endswith("-01 00:00:00"):
+                all_with_01_00_00_00 = False
+            if not str(cell_value).endswith("-01-01 00:00:00"):
+                all_with_01_01_00_00_00 = False
+        if all_with_01_01_00_00_00:
+            _header = df.columns[col_i]
+            df[_header] = df[_header].map(lambda x: x[:-len("-01-01 00:00:00")])
+            continue
+
+        if all_with_01_00_00_00:
+            _header = df.columns[col_i]
+            df[_header] = df[_header].map(lambda x: x[:-len("-01 00:00:00")])
+            continue
+
+        if all_with_00_00_00:
+            _header = df.columns[col_i]
+            df[_header] = df[_header].map(lambda x: x[:-len(" 00:00:00")])
+            continue
+
+    # Do header and cell value lower case
+    if lower_case:
+        new_columns = []
+        for header in df.columns:
+            lower_header = str(header).lower()
+            if lower_header in new_columns:
+                new_header, suffix = lower_header, 2
+                while new_header in new_columns:
+                    new_header = lower_header + '-' + str(suffix)
+                    suffix += 1
+                new_columns.append(new_header)
+            else:
+                new_columns.append(lower_header)
+        df.columns = new_columns
+        for header in df.columns:
+            # df[header] = df[header].map(lambda x: str(x).lower())
+            df[header] = df[header].map(lambda x: str(x).lower().strip())
+
+    # Recognize header type
+    for header in df.columns:
+
+        float_able = False
+        int_able = False
+        datetime_able = False
+
+        # Recognize int & float type
+        try:
+            df[header].astype("float")
+            float_able = True
+        except:
+            pass
+
+        if float_able:
+            try:
+                if all(df[header].astype("float") == df[header].astype(int)):
+                    int_able = True
+            except:
+                pass
+
+        if float_able:
+            if int_able:
+                df[header] = df[header].astype(int)
+            else:
+                df[header] = df[header].astype(float)
+
+        # Recognize datetime type
+        try:
+            df[header].astype("datetime64")
+            datetime_able = True
+        except:
+            pass
+
+        if datetime_able:
+            df[header] = df[header].astype("datetime64")
+
+    return df
+
 def passage_prompt(passages, only_title):
     if len(passages) == 0:
       return ""
